31ecb5ddcc1e798a7f1aad4f0b66a551349a4bcb
Freedy Carvalho <freedy.carvalho@gmail.com>
2020-05-12 03:54:36 -0300

    31. Implementando o carrinho de compras

--- Startup.cs
+++ Startup.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LanchesMacCurso.Context;
+using LanchesMacCurso.Models;
 using LanchesMacCurso.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -29,12 +31,17 @@ namespace LanchesMacCurso
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
 
-            // o serviço será criado cada vez que for solicitado
-            // .AddScope -> criado uma vez por solicitação
-            // .AddSingleton -> Usado na primeira vez que for solicitado
+            // .AddTransient -> o serviço será criado cada vez que for solicitado
+            // .AddScope -> criado uma vez por solicitação (cada requisição). Se duas pessoas solicitarem ao mesmo
+            // tempo, cada uma terá o seu carrinho e não teremos carrinhos duplicados
+            // .AddSingleton -> Usado na primeira vez que for solicitado (todas as requisições tenho o mesmo objeto)
             // services.AddTransient<Interface, Implementação>
             services.AddTransient<ICategoriaRepository, CategoriaRepository>();
             services.AddTransient<ILancheRepository, LancheRepository>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddScoped(cp => CarrinhoCompra.GetCarrinho(cp));
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,8 +57,18 @@ namespace LanchesMacCurso
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+            // Recurso para ativar o redirecionamento de URL
             app.UseHttpsRedirection();
+            
+            // Recurso para acessar os arquivos estáticos
             app.UseStaticFiles();
+
+            // Ativamos a session usando o MiddleWare
+            // No Asp Net Core tudo trabalha com o conceito de MiddleWare 
+            // Precisamos ativar para usar o recurso. Se não ativamos, a aplicação entende
+            // que o recurso não existe
+            app.UseSession(); 
 
             app.UseRouting();
 
