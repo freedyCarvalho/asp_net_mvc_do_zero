6d50d0d1bc81bc66e0716e014c374389f9e39475
Freedy Carvalho <freedy.carvalho@gmail.com>
2020-05-19 22:25:00 -0300

    37. Criando uma TagHelper

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMacCurso.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
