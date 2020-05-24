e8eecb1ce110da2a45e09c3426ce6dfe54ebeae9
Freedy Carvalho <freedy.carvalho@gmail.com>
2020-05-24 19:20:23 -0300

    44. Procurando Lanches - Search

--- Controllers/LancheController.cs
+++ Controllers/LancheController.cs
@@ -91,6 +91,22 @@ namespace LanchesMacCurso.Controllers
 
             return View(lanche);
         }
+
+        public IActionResult Search(string searchString)
+        {
+            string _searchString = searchString;
+            IEnumerable<Lanche> lanches;
+            string _categoriaAtual = string.Empty;
+
+            if (string.IsNullOrEmpty(_searchString))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.Nome);
+            } else
+            {
+                lanches = _lancheRepository.Lanches.Where(l => l.Nome.ToLower().Contains(_searchString.ToLower()));
+            }
+            return View("~/Views/Lanche/List.cshtml", new LancheListViewModels { Lanches = lanches, CategoriaAtual = _categoriaAtual });
+        }
     
     }
 }
