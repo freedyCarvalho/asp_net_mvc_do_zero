77de7e9009e9fe3ba80449e63f0588b3696a0920
Freedy Carvalho <freedy.carvalho@gmail.com>
2020-05-30 17:01:10 -0300

    59. Ajuste para melhorar a segurança I e II e para a versão Core 3.1

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LanchesMacCurso.Extensao;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,7 +14,13 @@ namespace LanchesMacCurso
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+
+            //criamos o método de extensão CreateAdminRole
+            CreateHostBuilder(args)
+               .Build()
+               .CreateAdminRole()
+               .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
